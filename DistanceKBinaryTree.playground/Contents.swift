42c56421ce98f56fda21beb7e6fbde5cd876699c
Kaan Ersan <kanersan@gmail.com>
2020-04-05 14:10:08 -0400

    initial commit

---
+++ DistanceKBinaryTree.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+import PlaygroundSupport
+
+
+
+
+let distanceK = UIImage(named: "sketch0.png")
+
+/// Given a binary tree with Root, and a Target node. Find all the nodes that are K distance away from the Target node.
+///             3
+///           /    \
+///          5       1
+///         / \      / \
+///        6  2     0   8
+///          / \
+///         7   4
+
+
+func distanceK(_ root: )
